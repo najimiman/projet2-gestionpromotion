3f36eefe9ea98752f091f5d0c964520fe24fe44c
najimiman <114479872+najimiman@users.noreply.github.com>
2022-10-25 07:12:27 -0700

    delete

--- projet2-réalisation/storage/framework/views/2c8f9fc3c423977e35fced067ead54da556fcafc.php
+++
@@ -1,9 +0,0 @@
-<form action="<?php echo e(Route('gestionstud.store')); ?>" method="POST">
-<?php echo csrf_field(); ?>
-first name:<input type="text" name="First_name">
-last name:<input type="text" name="Last_name">
-email:<input type="email" name="Email">
-<input type="text" name="PromotionID" value="<?php echo e($id); ?>">
-
-<button type="submit">ajouter</button>
-</form><?php /**PATH C:\xampp\htdocs\projet2-réalisation\resources\views/gestion/insert.blade.php ENDPATH**/ ?>
